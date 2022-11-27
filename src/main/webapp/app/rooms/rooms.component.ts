6022e2ca28d41dab68cea2a1e837176d7ea80ffc
dave <david.jaeger10@gmail.com>
2022-11-27 15:11:25 +0100

    moved rooms and updated navigation

--- src/main/webapp/app/rooms/rooms.component.ts
+++ src/main/webapp/app/rooms/rooms.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { rooms } from 'app/app.constants';
 
 @Component({
   selector: 'jhi-home',
@@ -7,50 +8,10 @@ import { Router } from '@angular/router';
   styleUrls: ['./rooms.component.scss'],
 })
 export class RoomsComponent {
+  rooms = rooms;
 
-  rooms = [
-    {
-      id: 1,
-      title: 'Luxury Sweet',
-      image: 'luxury.jpg',
-      price: '379€',
-      description: 'Unsere Luxury Sweet im obersten Stockwerk bietet Ihnen ein King-Size Bett, als auch einen Whirlpool zum entspanne. Auf Ihrem großen Balkon haben Sie einen wunderschönen Blick auf das Meer.'
-    },
-    {
-      id: 2,
-      title: 'Presidentschal Sweet',
-      image: 'presidential.jpg',
-      price: '280€',
-      description: 'Unsere Presidentschal Sweet bietet Ihnen ein Queen-Size Bett, als auch direkten Blick zum Meer.'
-    },
-    {
-      id: 3,
-      title: 'Doppelzimmer Meerblick',
-      image: 'dz-sea.jpg',
-      price: '140€',
-      description: 'Doppelzimmer mit Blick aufs Meer, wo wüst Meer?'
-    },
-    {
-      id: 4,
-      title: 'Doppelzimmer ohne Meerblick',
-      image: 'dz-no-sea.jpg',
-      price: '110€',
-      description: 'Doppelzimmer ohne Blick aufs Meer, sicha wüst Meer!'
-    },
-    {
-      id: 5,
-      title: 'Einzelzimmer Meerblick',
-      image: 'ez-sea.jpg',
-      price: '90€',
-      description: 'Auweh nur a Einzelzimmer, findest schon noch wen brudi :*'
-    }
-  ]
+  constructor(
+    private router: Router
+  ) {}
 
-
-  constructor(private router: Router) {}
-
-
-  bookRoom(room: any): void {
-    this.router.navigate(['reservation/' + JSON.stringify(room.id)])
-  }
 }
