d38ade7925a3cd801960b0e21fed1f6dd5c87d63
dave <david.jaeger10@gmail.com>
2022-11-27 15:11:55 +0100

    created reservation component

---
+++ src/main/webapp/app/reservation/reservation.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRouteSnapshot } from '@angular/router';
+
+@Component({
+  selector: 'jhi-reservation',
+  templateUrl: './reservation.component.html',
+  styleUrls: ['./reservation.component.scss']
+})
+export class ReservationComponent implements OnInit {
+
+  constructor(
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
