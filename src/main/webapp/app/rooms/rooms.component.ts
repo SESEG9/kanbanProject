9e40552f6ef9d93de3e0588210455260f1ddae1d
dave <david.jaeger10@gmail.com>
2022-11-27 11:27:34 +0100

    updated styling

--- src/main/webapp/app/rooms/rooms.component.ts
+++ src/main/webapp/app/rooms/rooms.component.ts
@@ -10,30 +10,35 @@ export class RoomsComponent {
 
   rooms = [
     {
+      id: 1,
       title: 'Luxury Sweet',
       image: 'luxury.jpg',
       price: '379€',
       description: 'Unsere Luxury Sweet im obersten Stockwerk bietet Ihnen ein King-Size Bett, als auch einen Whirlpool zum entspanne. Auf Ihrem großen Balkon haben Sie einen wunderschönen Blick auf das Meer.'
     },
     {
+      id: 2,
       title: 'Presidentschal Sweet',
       image: 'presidential.jpg',
       price: '280€',
       description: 'Unsere Presidentschal Sweet bietet Ihnen ein Queen-Size Bett, als auch direkten Blick zum Meer.'
     },
     {
+      id: 3,
       title: 'Doppelzimmer Meerblick',
       image: 'dz-sea.jpg',
       price: '140€',
       description: 'Doppelzimmer mit Blick aufs Meer, wo wüst Meer?'
     },
     {
+      id: 4,
       title: 'Doppelzimmer ohne Meerblick',
       image: 'dz-no-sea.jpg',
       price: '110€',
       description: 'Doppelzimmer ohne Blick aufs Meer, sicha wüst Meer!'
     },
     {
+      id: 5,
       title: 'Einzelzimmer Meerblick',
       image: 'ez-sea.jpg',
       price: '90€',
@@ -46,6 +51,6 @@ export class RoomsComponent {
 
 
   bookRoom(room: any): void {
-    this.router.navigate(['reserveRoom/' + JSON.stringify(room.id)])
+    this.router.navigate(['reservation/' + JSON.stringify(room.id)])
   }
 }
