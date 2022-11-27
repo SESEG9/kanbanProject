46aeec270dcb2c67cc4adf042dbb520ca1409ccc
dave <david.jaeger10@gmail.com>
2022-11-27 10:29:55 +0100

    added home old module

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -25,12 +25,14 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
 import { ErrorComponent } from './layouts/error/error.component';
+import { HomeOldModule } from './home-old/home-old.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     SharedModule,
     HomeModule,
+    HomeOldModule,
     // jhipster-needle-angular-add-module JHipster will add new module here
     AppRoutingModule,
     // Set this to true to enable service worker (PWA)
