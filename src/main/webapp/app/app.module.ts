0721011819d5af818820345566e6274eeeef5d7d
Thomas Stoiber <e11777755@student.tuwien.ac.at>
2022-11-29 11:09:56 +0100

    #SSE-15 Create basic structure for new components

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -25,6 +25,7 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
 import { ErrorComponent } from './layouts/error/error.component';
+import { RoomCreateComponent } from './rooms/room.create/room.create.component';
 
 @NgModule({
   imports: [
@@ -45,7 +46,15 @@ import { ErrorComponent } from './layouts/error/error.component';
     { provide: NgbDateAdapter, useClass: NgbDateDayjsAdapter },
     httpInterceptorProviders,
   ],
-  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
+  declarations: [
+    MainComponent,
+    NavbarComponent,
+    ErrorComponent,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    FooterComponent,
+    RoomCreateComponent,
+  ],
   bootstrap: [MainComponent],
 })
 export class AppModule {
---
+++ src/main/webapp/app/rooms/room.create/room.create.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-room.create',
+  templateUrl: './room.create.component.html',
+  styleUrls: ['./room.create.component.scss'],
+})
+export class RoomCreateComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
