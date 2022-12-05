43494911998bc1744e27db9cec2d13e7b0487918
Thomas Stoiber <e11777755@student.tuwien.ac.at>
2022-12-05 19:17:54 +0100

    #SSE-15 Extend validation and display errors for capacity and price

--- src/main/webapp/app/entities/room/create/room.create.component.ts
+++ src/main/webapp/app/entities/room/create/room.create.component.ts
@@ -3,6 +3,7 @@ import { IRoomCapacity } from '../../room-capacity/room-capacity.model';
 import { EntityArrayResponseType, RoomCapacityService } from '../../room-capacity/service/room-capacity.service';
 import { Room, RoomPicture, RoomPrice } from '../room.model';
 import { RoomService } from '../service/room.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'jhi-create',
@@ -10,9 +11,11 @@ import { RoomService } from '../service/room.service';
   styleUrls: ['./room.create.component.scss'],
 })
 export class RoomCreateComponent implements OnInit {
-  constructor(private roomCapacityService: RoomCapacityService, private roomService: RoomService) {}
+  constructor(private roomCapacityService: RoomCapacityService, private roomService: RoomService, private modalService: NgbModal) {}
 
-  prices: RoomPrice[] = [{ id: null, capacity: undefined, price: 0 }];
+  prices: (RoomPrice & { capacityError: string; priceError: string })[] = [
+    { id: null, capacity: undefined, price: 0, capacityError: '', priceError: '' },
+  ];
 
   capacities: IRoomCapacity[] = [];
 
@@ -21,7 +24,7 @@ export class RoomCreateComponent implements OnInit {
 
   maxWeight: number = 0;
 
-  maxCapacity: number | null = null;
+  maxCapacity: number | null = 1;
   identifier: string = '';
 
   ngOnInit(): void {
@@ -33,7 +36,7 @@ export class RoomCreateComponent implements OnInit {
   }
 
   addNewPrice() {
-    this.prices.push({ id: null, capacity: undefined, price: 0 });
+    this.prices.push({ id: null, capacity: undefined, price: 0, capacityError: '', priceError: '' });
   }
 
   onFileSelected(event: any) {
@@ -61,17 +64,62 @@ export class RoomCreateComponent implements OnInit {
     this.imageAsBase64.splice(index, 1);
   }
 
+  private isFormValid(): boolean {
+    return (
+      this.validateIdentifier(this.identifier) &&
+      this.validateMaxCapacity(this.maxCapacity) &&
+      this.prices.every(p => this.validatePrice(p)) &&
+      this.prices.every(p => this.validatePriceCapacity(p))
+    );
+  }
+
+  validateIdentifier(identifier: string): boolean {
+    return identifier.length > 0;
+  }
+
+  validateMaxCapacity(maxCapacity: number | null): boolean {
+    return maxCapacity != null && maxCapacity >= 1 && Math.trunc(maxCapacity) == maxCapacity;
+  }
+
+  validatePrice(price: RoomPrice & { priceError: string }): boolean {
+    if (price.price <= 0) {
+      price.priceError = 'Preis muss größer 0 sein!';
+      return false;
+    } else {
+      price.priceError = '';
+      return true;
+    }
+  }
+
+  validatePriceCapacity(price: RoomPrice & { capacityError: string }): boolean {
+    if (price.capacity?.capacity == undefined) {
+      price.capacityError = 'Belegung wählen!';
+      return false;
+    } else if (this.maxCapacity != null && price.capacity.capacity > this.maxCapacity) {
+      price.capacityError = 'Belegung zu groß für Maximalkapazität!';
+      return false;
+    } else if (this.prices.filter(p => p.capacity != null).filter(p => p.capacity?.id == price.capacity?.id).length > 1) {
+      price.capacityError = 'Belegung darf nur einmal gewählt werden!';
+      return false;
+    } else {
+      price.capacityError = '';
+      return true;
+    }
+  }
+
   submitRoom() {
-    const room: Room = {
-      id: null,
-      maxCapacity: this.maxCapacity,
-      identifyer: this.identifier,
-      prices: this.prices,
-      roomPictures: this.imageAsBase64,
-    };
-
-    console.log(room);
-
-    this.roomService.create(room).subscribe(next => console.log(next));
+    if (this.isFormValid()) {
+      const room: Room = {
+        id: null,
+        maxCapacity: this.maxCapacity,
+        identifyer: this.identifier,
+        prices: this.prices,
+        roomPictures: this.imageAsBase64,
+      };
+      this.roomService.create(room).subscribe({
+        next: next => console.log(next),
+        error: error => {},
+      });
+    }
   }
 }
