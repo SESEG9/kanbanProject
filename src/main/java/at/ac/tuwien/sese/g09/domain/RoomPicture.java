1592fe62d05355b1dcf31f46faf8484206925c48
Thomas Stoiber <e11777755@student.tuwien.ac.at>
2022-12-07 17:29:49 +0100

    SSE-67 Make Picture in Room Picture to Lob

--- src/main/java/at/ac/tuwien/sese/g09/domain/RoomPicture.java
+++ src/main/java/at/ac/tuwien/sese/g09/domain/RoomPicture.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
@@ -22,7 +23,7 @@ public class RoomPicture {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "image")
+    @Lob
     private String image;
 
     @Column(name = "weight")
