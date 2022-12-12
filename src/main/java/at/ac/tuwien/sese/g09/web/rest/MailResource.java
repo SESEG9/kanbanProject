2fd7e0a7c78a5d036c2c8349aba763b717c0822f
Simon Schwantler <simon.schwantler@powerbot-trading.com>
2022-12-12 12:22:46 +0100

    fix controller

--- src/main/java/at/ac/tuwien/sese/g09/web/rest/MailResource.java
+++ src/main/java/at/ac/tuwien/sese/g09/web/rest/MailResource.java
@@ -1,19 +1,16 @@
 package at.ac.tuwien.sese.g09.web.rest;
 
-import at.ac.tuwien.sese.g09.domain.Booking;
-import at.ac.tuwien.sese.g09.repository.BookingRepository;
-import at.ac.tuwien.sese.g09.service.BookingService;
 import at.ac.tuwien.sese.g09.service.MailService;
-import at.ac.tuwien.sese.g09.service.dto.BookingDTO;
-import java.net.URISyntaxException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api")
 public class MailResource {
 
     private final Logger log = LoggerFactory.getLogger(BookingResource.class);
