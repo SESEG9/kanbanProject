fd874c91b353a9d0c2ba69fa47be7cf9fe8d5d67
Thomas Stoiber <e11777755@student.tuwien.ac.at>
2023-01-04 16:58:06 +0100

    SSE-78 Fix Linting

--- src/main/webapp/app/entities/vacation/vacation-apply-check/vacation-apply-check.component.ts
+++ src/main/webapp/app/entities/vacation/vacation-apply-check/vacation-apply-check.component.ts
@@ -14,68 +14,72 @@ export class VacationApplyCheckComponent implements OnInit {
     freeVacation: 10,
   };
 
-  vacation: VacationApply = {
-    from: new Date('2023-02-01'),
-    to: new Date('2023-02-04'),
-    state: 'APPLIED',
-  };
+  vacation: VacationApply;
 
-  overlappings: VacationApplyUser[] = [
-    {
-      from: new Date('2023-02-01'),
-      to: new Date('2023-02-04'),
-      state: 'APPLIED',
-      id: 15,
-      user: {
-        firstName: 'Jürgen',
-        lastName: 'Müller',
-        area: 'Rezeption',
-        freeVacation: 10,
-      },
-    },
-    {
-      from: new Date('2023-02-01'),
-      to: new Date('2023-02-15'),
-      state: 'APPLIED',
-      id: 15,
-      user: {
-        firstName: 'Sandra',
-        lastName: 'Kegler',
-        area: 'Technik',
-        freeVacation: 10,
-      },
-    },
-    {
-      from: new Date('2023-01-30'),
-      to: new Date('2023-02-06'),
-      state: 'APPLIED',
-      id: 15,
-      user: {
-        firstName: 'Anja',
-        lastName: 'Löwe',
-        area: 'Rezeption',
-        freeVacation: 10,
-      },
-    },
-    {
-      from: new Date('2023-01-25'),
-      to: new Date('2023-02-01'),
-      state: 'APPLIED',
-      id: 15,
-      user: {
-        firstName: 'Annemarie',
-        lastName: 'Stöger',
-        area: 'Cleaning',
-        freeVacation: 10,
-      },
-    },
-  ];
+  overlappings: VacationApplyUser[] = [];
 
   datediff(first: Date, second: Date): number {
     return Math.round((second.getTime() - first.getTime()) / (1000 * 60 * 60 * 24)) + 1;
   }
 
-  constructor() {}
+  constructor() {
+    this.vacation = {
+      from: new Date('2023-02-01'),
+      to: new Date('2023-02-04'),
+      state: 'APPLIED',
+    };
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.overlappings = [
+      {
+        from: new Date('2023-02-01'),
+        to: new Date('2023-02-04'),
+        state: 'APPLIED',
+        id: 15,
+        user: {
+          firstName: 'Jürgen',
+          lastName: 'Müller',
+          area: 'Rezeption',
+          freeVacation: 10,
+        },
+      },
+      {
+        from: new Date('2023-02-01'),
+        to: new Date('2023-02-15'),
+        state: 'APPLIED',
+        id: 15,
+        user: {
+          firstName: 'Sandra',
+          lastName: 'Kegler',
+          area: 'Technik',
+          freeVacation: 10,
+        },
+      },
+      {
+        from: new Date('2023-01-30'),
+        to: new Date('2023-02-06'),
+        state: 'APPLIED',
+        id: 15,
+        user: {
+          firstName: 'Anja',
+          lastName: 'Löwe',
+          area: 'Rezeption',
+          freeVacation: 10,
+        },
+      },
+      {
+        from: new Date('2023-01-25'),
+        to: new Date('2023-02-01'),
+        state: 'APPLIED',
+        id: 15,
+        user: {
+          firstName: 'Annemarie',
+          lastName: 'Stöger',
+          area: 'Cleaning',
+          freeVacation: 10,
+        },
+      },
+    ];
+  }
 }
