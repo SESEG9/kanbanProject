2897abe615a752f6e966d21a5ad8e6c1caad7d1d
Thomas Stoiber <e11777755@student.tuwien.ac.at>
2023-01-05 10:20:36 +0100

    SSE-78 Add Modals to check component and rederict to parent page if successful

--- src/main/webapp/app/entities/vacation/vacation-apply-check/vacation-apply-check.component.ts
+++ src/main/webapp/app/entities/vacation/vacation-apply-check/vacation-apply-check.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { VacationApply, VacationApplyUser } from '../vacation.model';
+import { VacationApplyUser } from '../vacation.model';
 import { VacationDateService } from '../service/vacation-date.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { VacationRejectDialogComponent } from '../dialog-reject/vacation-reject-dialog.component';
+import { filter } from 'rxjs';
+import { VACATION_APPROVED, VACATION_REJECTED } from '../vacation.constants';
+import { EntityArrayResponseType } from '../service/vacation.service';
+import { VacationApproveDialogComponent } from '../dialog-approve/vacation-approve-dialog.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'jhi-vacation-apply-check',
@@ -9,29 +15,61 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./vacation-apply-check.component.scss', '../../room/room.global.scss'],
 })
 export class VacationApplyCheckComponent implements OnInit {
-  user = {
-    firstName: 'Hans',
-    lastName: 'Meier',
-    area: 'Administration',
-    freeVacation: 10,
-  };
-
-  vacation: VacationApply;
+  vacation: VacationApplyUser | null = null;
 
   overlappings: VacationApplyUser[] = [];
 
-  param: any = 0;
+  constructor(
+    public vacationDateService: VacationDateService,
+    private route: ActivatedRoute,
+    protected modalService: NgbModal,
+    private router: Router
+  ) {}
 
-  constructor(public vacationDateService: VacationDateService, private route: ActivatedRoute) {
-    this.vacation = {
-      from: new Date('2023-02-01'),
-      to: new Date('2023-02-04'),
-      state: 'APPLIED',
-    };
+  reject(vacation: VacationApplyUser | null): void {
+    if (vacation != null) {
+      const modalRef = this.modalService.open(VacationRejectDialogComponent, { backdrop: 'static' });
+      modalRef.componentInstance.vacation = vacation;
+      // unsubscribe not needed because closed completes on modal close
+
+      modalRef.closed.pipe(filter(reason => reason === VACATION_REJECTED)).subscribe({
+        next: (res: EntityArrayResponseType) => {
+          this.router.navigateByUrl('/vacation');
+        },
+      });
+    }
+  }
+
+  approve(vacation: VacationApplyUser | null): void {
+    if (vacation != null) {
+      const modalRef = this.modalService.open(VacationApproveDialogComponent, { backdrop: 'static' });
+      modalRef.componentInstance.vacation = vacation;
+      // unsubscribe not needed because closed completes on modal close
+
+      modalRef.closed.pipe(filter(reason => reason === VACATION_APPROVED)).subscribe({
+        next: (res: EntityArrayResponseType) => {
+          this.router.navigateByUrl('/vacation');
+        },
+      });
+    }
   }
 
   ngOnInit(): void {
-    this.route.params.subscribe(param => (this.param = +param.id));
+    this.route.params.subscribe(
+      param =>
+        (this.vacation = {
+          from: new Date('2023-02-01'),
+          to: new Date('2023-02-04'),
+          state: 'APPLIED',
+          user: {
+            firstName: 'Hans',
+            lastName: 'Meier',
+            area: 'Administration',
+            freeVacation: 10,
+          },
+          id: +param.id,
+        })
+    );
     // TODO use this parameter to load the data from the backend
 
     this.overlappings = [
